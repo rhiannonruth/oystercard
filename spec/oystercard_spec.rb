55eec2116ec54e2fc50a492122b1cc5835dbce54
Rhiannon Lolley Neville <rhiannonruth@hotmail.co.uk>
2016-03-07 16:45:46 +0000

    Updated .rspec with format documentation and added methods and test for balance on oystercard

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,12 @@ require 'oystercard'
 
 describe Oystercard do
 
+  subject(:oystercard) {described_class.new}
+
+  describe '#balance' do
+    it 'initializes with an empty balance' do
+      expect(oystercard.balance).to eq(0)
+    end
+  end
+
 end
