2882b0249b1c72325393c551463687e7db3a0675
Chris Parker <cjp214@gmail.com>
2016-03-07 17:12:31 +0000

    made the code arid

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,16 @@
 class Oystercard
 
   attr_reader :balance
-  MAX_BALANCE_ERROR = "maximum balance exceeded"
+
+  MAX_BALANCE = 90
+  MAX_BALANCE_ERROR = "maximum balance is £#{MAX_BALANCE}"
 
   def initialize
     @balance = 0
   end
 
   def top_up amount
-    fail MAX_BALANCE_ERROR if (balance + amount) > 90
+    fail MAX_BALANCE_ERROR if (balance + amount) > MAX_BALANCE
     @balance += amount
   end
 end
