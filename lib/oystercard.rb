3661263cf001d464211a22d8d76e72aa44b2c22f
adil ali <adiln1993@gmail.com>
2016-03-08 09:22:46 +0000

    challenge #7

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,16 @@
 class Oystercard
+  LIMIT = 90
   attr_reader :balance
   def initialize
     @balance = 0
   end
 
+  def top_up(amount)
+    raise "Sorry, balance limit is #{LIMIT}£" if (@balance + amount) > LIMIT
+    @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,26 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
+  describe "#top_up" do
+    it "should top up card by specified amount" do
+      subject.top_up(10)
+      expect(subject.balance).to eq 10
+    end
+
+    it "should raise an error if amount topped up makes the balance exceed 90£" do
+      subject.top_up(10)
+      expect{subject.top_up(90)}.to raise_error("Sorry, balance limit is 90£")
+    end
+  end
+
+  describe "#deduct" do
+    it "should deduct the specified amount from balance" do
+      subject.top_up(10)
+      subject.deduct(10)
+      expect(subject.balance).to eq 0
+
+    end
+  end
+
+
 end
