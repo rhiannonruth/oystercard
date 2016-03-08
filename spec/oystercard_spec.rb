70f06d63023227f9575e2484538bf21a4ac6f0eb
Jazzy <jasmina.gasper@gmail.com>
2016-03-08 10:58:02 +0000

    deduct method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,23 +2,30 @@ require 'oystercard'
 
 describe Oystercard do
 
-subject(:card) {described_class.new}
+subject(:card) { described_class.new }
 
-  describe 'balance' do
+  describe '#balance' do
 
     it 'starts with a balance of 0' do
       expect(card.check_balance).to eq 0
     end
   end
 
-  describe 'Top-up' do
-    it 'it should return the value it was top-uped with' do
+  describe '#top-up' do
+    it 'it should change the balance it was top-uped with' do
       card.top_up(20)
       expect(card.check_balance).to eq 20
     end
     it 'raises error when topped up over the limit' do
       card.top_up(Oystercard::MAX_LIMIT)
-      expect {card.top_up(1)}.to raise_error Oystercard::MAX_LIMIT_ERROR
+      expect { card.top_up(1) }.to raise_error Oystercard::MAX_LIMIT_ERROR
+    end
+  end
+
+  describe '#deduct' do
+    it 'it should deduct given value from the balance' do
+      card.top_up(10)
+      expect{ card.deduct(5) }.to change{ card.check_balance }.from(10).to(5)
     end
   end
 
