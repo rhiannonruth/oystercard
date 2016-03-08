2bc1cff3254571902720c591fc9458350e26fc77
adil ali <adiln1993@gmail.com>
2016-03-08 10:28:30 +0000

    challenge #8

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,16 +1,29 @@
 class Oystercard
-  LIMIT = 90
+  MAXIMUM_BALANCE = 90
   attr_reader :balance
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
-    raise "Sorry, balance limit is #{LIMIT}£" if (@balance + amount) > LIMIT
+    raise "Maximum balance exceeded" if (@balance + amount) > MAXIMUM_BALANCE
     @balance += amount
   end
 
   def deduct(amount)
     @balance -= amount
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,29 +1,44 @@
 require 'oystercard'
 
 describe Oystercard do
-
+  subject(:oystercard) {described_class.new}
   it 'is initialized with a balance of 0' do
-    expect(subject.balance).to eq 0
+    expect(oystercard.balance).to eq 0
   end
 
   describe "#top_up" do
     it "should top up card by specified amount" do
-      subject.top_up(10)
-      expect(subject.balance).to eq 10
+      expect{oystercard.top_up(10)}.to change{oystercard.balance}.by(10)
     end
 
-    it "should raise an error if amount topped up makes the balance exceed 90£" do
-      subject.top_up(10)
-      expect{subject.top_up(90)}.to raise_error("Sorry, balance limit is 90£")
+    it "should raise an error if maximum balance exceeded" do
+      amount = described_class::MAXIMUM_BALANCE + 1
+      expect{oystercard.top_up(amount)}.to raise_error("Maximum balance exceeded")
     end
   end
 
   describe "#deduct" do
     it "should deduct the specified amount from balance" do
-      subject.top_up(10)
-      subject.deduct(10)
-      expect(subject.balance).to eq 0
+      expect{oystercard.deduct(10)}.to change{oystercard.balance}.by(-10)
+    end
+  end
 
+  describe "#in_journey?" do
+    it "should display travel status" do
+      expect(oystercard).not_to be_in_journey
+    end
+  end
+
+  describe "#touch_in" do
+    it "should change in_journey status to true" do
+      expect{oystercard.touch_in}.to change{oystercard.in_journey?}.to(true)
+    end
+  end
+
+  describe "#touch_out" do
+    it "should change in_journey status to false" do
+      oystercard.touch_in
+      expect{oystercard.touch_out}.to change{oystercard.in_journey?}.to(false)
     end
   end
 
