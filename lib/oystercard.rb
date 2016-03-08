35eb43860ccda3c55d06f0ee759548c551dd49e4
Junyuan Xue <junyuanxue.is@gmail.com>
2016-03-08 11:28:00 +0000

    Challenge 11. Created entry_station instance var

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,11 @@
 class Oystercard
+
   MAXIMUM_BALANCE = 90
   MINIMUM_FARE = 1
-  attr_reader :balance
+  attr_reader :balance, :entry_station
+
   def initialize
     @balance = 0
-    @in_journey = false
   end
 
   def top_up(amount)
@@ -13,17 +14,17 @@ class Oystercard
   end
 
   def in_journey?
-    @in_journey
+    @entry_station != nil
   end
 
-  def touch_in
+  def touch_in(station)
     raise "Not enough funds" if @balance < MINIMUM_FARE
-    @in_journey = true
+    @entry_station = station
   end
 
   def touch_out
-    @in_journey = false
     deduct(MINIMUM_FARE)
+    @entry_station = nil
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,10 +2,13 @@ require 'oystercard'
 
 describe Oystercard do
   subject(:oystercard) {described_class.new}
-  it 'is initialized with a balance of 0' do
-    expect(oystercard.balance).to eq 0
-  end
+  let(:station) {double :station}
 
+  describe "initialization" do
+    it 'is initialized with a balance of 0' do
+      expect(oystercard.balance).to eq 0
+    end
+  end
 
   describe "#top_up" do
     it "should top up card by specified amount" do
@@ -27,25 +30,36 @@ describe Oystercard do
   describe "#touch_in" do
     it "should change in_journey status to true" do
       oystercard.top_up(10)
-      expect{oystercard.touch_in}.to change{oystercard.in_journey?}.to(true)
+      expect{oystercard.touch_in(station)}.to change{oystercard.in_journey?}.to(true)
     end
 
     it "should raise error when under minimum balance" do
-      expect{oystercard.touch_in}.to raise_error "Not enough funds"
+      expect{oystercard.touch_in(station)}.to raise_error "Not enough funds"
+    end
+
+    it "should store the entry station" do
+      oystercard.top_up(10)
+      oystercard.touch_in(station)
+      expect(oystercard.entry_station).to eq station
     end
   end
 
   describe "#touch_out" do
     before do
       oystercard.top_up(10)
-      oystercard.touch_in
+      oystercard.touch_in(station)
     end
+
     it "should change in_journey status to false" do
       expect{oystercard.touch_out}.to change{oystercard.in_journey?}.to(false)
     end
 
     it "should deduct minimum fare from balance" do
       expect{oystercard.touch_out}.to change{oystercard.balance}.by(-Oystercard::MINIMUM_FARE)
+    end
+
+    it "should clear the entry station" do
+      expect{oystercard.touch_out}.to change{oystercard.entry_station}.to nil
     end
   end
 
