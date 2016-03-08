fb85c76485cc6338e7abeffe6e23c9b307ac4edc
Misa Ogura <misa.ogura01@gmail.com>
2016-03-08 11:00:59 +0000

    4: Add the balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,9 @@
 class Oystercard
+  DEFAULT_BALANCE = 5
+  attr_reader :balance
+
+  def initialize
+    @balance = DEFAULT_BALANCE
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,15 @@ require 'oystercard'
 
 describe Oystercard do
 
+  default_value = Oystercard::DEFAULT_BALANCE
+
+
+  subject(:card) { described_class.new }
+
+  describe '#initialize' do
+
+    it 'is created with a balance' do
+      expect(card.balance).to eq default_value
+    end
+  end
 end
